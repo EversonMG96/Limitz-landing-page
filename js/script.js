57fce79f778a95393092e1d6a60b30361218e6f3
Everson <138945004+EversonMG96@users.noreply.github.com>
2026-02-01 01:27:52 -0300

    criado javascript do projeto

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 const slides = document.querySelector('.carouselimg');
 let curentSlide = 0;
+
+setInterval(() => {
+    slides[currentSlide].classList.remove('active');
+    curentSlide = (curentSlide + 1) % slides.length;
+    slides[curentSlide].classList.add('active');
+});
